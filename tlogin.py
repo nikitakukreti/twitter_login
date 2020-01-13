800500b0297e50424dec57823ef2b784dfc23f50
nikitakukreti <53289761+nikitakukreti@users.noreply.github.com>
2020-01-13 15:58:36 +0530

    Add files via upload

---
+++ tlogin.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from getpass import getpass
+
+usr = input('Enter your username or email : ')
+pwd = getpass('Enter your password : ')
+
+driver = webdriver.Chrome(executable_path='C:\\Users/Nikita/Downloads/chromedriver_win32/chromedriver.exe')
+driver.get('https://twitter.com/login')
+usr_box = driver.find_element_by_class_name('js-username-field')
+usr_box.send_keys(usr)
+
+pwd_box = driver.find_element_by_class_name('js-password-field')
+pwd_box.send_keys(pwd)
+
+login_button = driver.find_element_by_css_selector('button.submit.EdgeButton.EdgeButton--primary.EdgeButtom--medium')
+login_button.submit()
